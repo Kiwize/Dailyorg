011b4a06001692ae89bc5b41d94eb80c1f4ac700
Thomas PRADEAU <pradeau.thomas0@gmail.com>
2025-08-21 12:35:04 +0200

    [FIX] Missing SpringExtension

--- eclipse-workspace/dailyorg_java/src/test/java/fr/nexa/dailyorg/service/dailyorg/impl/TaskServiceTest.java
+++ eclipse-workspace/dailyorg_java/src/test/java/fr/nexa/dailyorg/service/dailyorg/impl/TaskServiceTest.java
@@ -9,9 +9,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import fr.nexa.dailyorg.components.factory.dailyorg.RecurringTaskStateFactory;
 import fr.nexa.dailyorg.components.factory.dailyorg.TaskFactory;
@@ -20,6 +22,7 @@ import fr.nexa.dailyorg.repository.dailyorg.ITaskRepository;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
 public class TaskServiceTest {
 
 	private final ITaskRepository taskRepository;
